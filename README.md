6ecef8b93a4cd6f529961aef1003476b032692ba
Yubo Zhang <418436384@qq.com>
2020-10-11 10:40:18 +0800

    nothing

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# EasyADBCLI
-EasyADB CLI Version
+### EasyADBCLI
+A ~~powerful~~ ADB frontend writed by C++   
+
+**WARNING: THIS PROJECT ONLY FOR TEST PURPOSE,PLEASE DON'T MAKE IT ON YOUR PROJECTS**
+### Build:
+
+`cmake .`   
+
+
+Simplicity.
--- menu.h
+++ menu.h
@@ -11,6 +11,7 @@
 // menu.h:Main menu of EasyADBCLI.
 
 #include <iostream>
+#include <cstring>
 
 #ifndef EASYADBCLI_MENU_H
 #define EASYADBCLI_MENU_H
@@ -20,29 +21,30 @@
 
 class menu {
 public:
-    static void chinese() {
+    void chinese() {
         std::cout << "欢迎使用EasyADB CLI模式！" << std::endl
-             << "发布日期:" << __DATE__ << "，由" << __AUTHOR__ << "编写。" << std::endl
-             << "编译日期:" << __DATE__ << " " << __TIME__ << std::endl
-             << "版本 " << __REL_VERSION__ << "." << std::endl
-             << "选项:" << std::endl
-             << "1.安装apk" << std::endl
-             << "0.选择语言";
+                  << "发布日期:" << __DATE__ << "，由" << __AUTHOR__ << "编写。" << std::endl
+                  << "编译日期:" << __DATE__ << " " << __TIME__ << std::endl
+                  << "版本 " << __REL_VERSION__ << "." << std::endl
+                  << "选项:" << std::endl
+                  << "1.安装apk" << std::endl
+                  << "0.选择语言";
     }
 
-    static void english() {
+    void english() {
         std::cout << "Hello from EasyADB CLI mode!" << std::endl
-             << "Release at:" << __REL_DATE__ << ",by " << __AUTHOR__ << "." << std::endl
-             << "Compile at:" << __DATE__ << " " << __TIME__ << std::endl
-             << "Version " << __REL_VERSION__ << "." << std::endl
-             << "Select option:" << std::endl
-             << "1.Install package"
-             << "0.Switch language";
+                  << "Release at:" << __REL_DATE__ << ",by " << __AUTHOR__ << "." << std::endl
+                  << "Compile at:" << getCompileDate() << " " << __TIME__ << std::endl
+                  << "Version " << __REL_VERSION__ << "." << std::endl
+                  << "Select option:" << std::endl
+                  << "1.Install package"
+                  << "0.Switch language";
     }
+
     void languageChooser() {
         std::cout << "Choose language/选择语言" << std::endl
-             << "1.English" << std::endl
-             << "2.Chinese" << std::endl;
+                  << "1.English" << std::endl
+                  << "2.Chinese" << std::endl;
         int opt;
         std::cin >> opt;
         switch (opt) {
@@ -55,6 +57,23 @@ public:
             default:
                 break;
         }
+    }
+
+private:
+        void getCompileDateBase(uint8_t *Year, uint8_t *Month, uint8_t *Day) {
+        const char *pMonth[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        const char Date[12] = __DATE__;//取编译时间
+        uint8_t i;
+        for (i = 0; i < 12; i++)if (memcmp(Date, pMonth[i], 3) == 0)*Month = i + 1, i = 12;
+        *Year = (uint8_t) atoi(Date + 9); //Date[9]为２位年份，Date[7]为完整年份
+        *Day = (uint8_t) atoi(Date + 4);
+    }
+    char g_date_buf[10];
+    char *getCompileDate(void) {
+        uint8_t Year, Month, Day;
+        getCompileDateBase(&Year, &Month, &Day);//取编译时间
+        sprintf(g_date_buf, "%02d%02d%02d", Year, Month, Day);//任意格式化
+        return g_date_buf;
     }
 } menu;
 
