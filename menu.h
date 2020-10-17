d2a613ff073e543055ca7879b9f6ee292498b789
Yubo Zhang <418436384@qq.com>
2020-10-17 22:49:37 +0800

    nothing

--- menu.h
+++ menu.h
@@ -16,16 +16,14 @@
 #ifndef EASYADBCLI_MENU_H
 #define EASYADBCLI_MENU_H
 #define __AUTHOR__ "nu11plex"
-#define __REL_DATE__ "2020/10/8"
-#define __REL_VERSION__ 0.01
 
 class menu {
 public:
     void chinese() {
         std::cout << "欢迎使用EasyADB CLI模式！" << std::endl
-                  << "发布日期:" << __DATE__ << "，由" << __AUTHOR__ << "编写。" << std::endl
-                  << "编译日期:" << __DATE__ << " " << __TIME__ << std::endl
-                  << "版本 " << __REL_VERSION__ << "." << std::endl
+                  << "发布日期:" << getCompileDate()<< "，由" << __AUTHOR__ << "编写。" << std::endl
+                  << "编译日期:" << getCompileDate() << " " << __TIME__ << std::endl
+                  << "版本 " << getReleaseVersion() << "." << std::endl
                   << "选项:" << std::endl
                   << "1.安装apk" << std::endl
                   << "0.选择语言";
@@ -33,12 +31,12 @@ public:
 
     void english() {
         std::cout << "Hello from EasyADB CLI mode!" << std::endl
-                  << "Release at:" << __REL_DATE__ << ",by " << __AUTHOR__ << "." << std::endl
+                  << "Release at:" << getCompileDate() << ",by " << __AUTHOR__ << "." << std::endl
                   << "Compile at:" << getCompileDate() << " " << __TIME__ << std::endl
-                  << "Version " << __REL_VERSION__ << "." << std::endl
+                  << "Version " << getReleaseVersion() << "." << std::endl
                   << "Select option:" << std::endl
-                  << "1.Install package"
-                  << "0.Switch language";
+                  << "1.Install package" << std::endl
+                  << "0.Switch language" << std::endl ;
     }
 
     void languageChooser() {
@@ -62,17 +60,23 @@ public:
 private:
         void getCompileDateBase(uint8_t *Year, uint8_t *Month, uint8_t *Day) {
         const char *pMonth[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        const char Date[12] = __DATE__;//取编译时间
+        const char Date[12] = __DATE__;
         uint8_t i;
         for (i = 0; i < 12; i++)if (memcmp(Date, pMonth[i], 3) == 0)*Month = i + 1, i = 12;
-        *Year = (uint8_t) atoi(Date + 9); //Date[9]为２位年份，Date[7]为完整年份
+        *Year = (uint8_t) atoi(Date + 9);
         *Day = (uint8_t) atoi(Date + 4);
     }
     char g_date_buf[10];
     char *getCompileDate(void) {
         uint8_t Year, Month, Day;
-        getCompileDateBase(&Year, &Month, &Day);//取编译时间
-        sprintf(g_date_buf, "%02d%02d%02d", Year, Month, Day);//任意格式化
+        getCompileDateBase(&Year, &Month, &Day);
+        sprintf(g_date_buf, "%02d20/%02d/%02d", Year, Month, Day);
+        return g_date_buf;
+    }
+    char *getReleaseVersion(void){
+        uint8_t Year, Month, Day;
+        getCompileDateBase(&Year, &Month, &Day);
+        sprintf(g_date_buf, "1.%02d.%02d", Month, Day);
         return g_date_buf;
     }
 } menu;
